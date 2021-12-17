dd5876320905c908f5852a50356fee7ac6ff0bc7
yatharth kapadia <yatharth.kapadia.1702@gmail.com>
2021-12-18 03:25:03 +0530

    Dqn init

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 import gym
 import highway_env
 from matplotlib import pyplot as plt
-env = gym.make('highway-v0')
+env = gym.make("roundabout-v0")
 env.reset()
-for _ in range(30):
+for _ in range(300):
     action = env.action_type.actions_indexes["IDLE"]
     obs, reward, done, info = env.step(action)
     env.render()
 plt.imshow(env.render(mode="rgb_array"))
 plt.show()
-
-env = gym.make("highway-v0")
